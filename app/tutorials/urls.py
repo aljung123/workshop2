012849a6e2dada8464a17c7c48cc05d801cefcd7
Anders Ljungholm <andersljungholm17@gmail.com>
2022-08-23 21:21:43 +0200

    second commit

--- app/tutorials/urls.py
+++ app/tutorials/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from tutorials import views as tutorials_views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import include
+from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
